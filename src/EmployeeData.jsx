5a560dcb7592ef7d3fa2aa703623154df04d0e8c
Aman kumar <138058847+aman2world@users.noreply.github.com>
2024-04-05 14:51:29 +0530

    crud

---
+++ src/EmployeeData.jsx
@@ -0,0 +1,23 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: 'Aman',
+        lastName: 'Kumar',
+        age: 24
+    },
+    {
+        id: 2,
+        firstName: 'Sohail',
+        lastName: 'khan',
+        age: 19
+    },
+    {
+        id: 3,
+        firstName: 'Ankit',
+        lastName: 'manjhi',
+        age: 22
+    },
+]
+
+
+
